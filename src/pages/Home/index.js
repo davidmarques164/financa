0a4a60ed0d57545178f2023a411d28f048b273eb
davidmarques164 <davidpeterpaulmarques@gmail.com>
2023-03-20 02:17:53 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,69 @@
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import Header from '../../components/Header'
+import Balance from '../../components/Balance';
+import Movements from '../../components/Movements';
+import Actions from '../../components/Actions';
+
+const  list = [
+    {
+        id: 1,
+        label: 'Boleto conta luz',
+        value: '300,30',
+        date: '17/09/2023',
+        type: 2
+    },
+    {
+        id: 2,
+        label: 'Boleto conta agua',
+        value: '300,30',
+        date: '17/09/2023',
+        type: 1
+    },
+    {
+        id: 3,
+        label: 'Boleto conta pix',
+        value: '300,30',
+        date: '17/09/2023',
+        type: 1
+    }
+]
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Header name="David Peter"/>
+
+      <Balance saldo="9.250,90" gastos="-527,00"/>
+
+      <Actions/>
+
+      <Text style={styles.title}>Últimas movimentações</Text>
+
+      <FlatList
+        style={styles.list}
+        data={list}
+        keyExtractor={(item) => String(item.id)}
+        showVerticalScrollIndicator={false}
+        renderItem={ ({ item }) =>  <Movements data={item}/> }
+      />
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',    
+  },
+  title:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    margin: 14,
+  },
+  list:{
+    marginStart: 14,
+    marginEnd: 14,
+  }
+});
